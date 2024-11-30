0304d7c298332478e94f5863158080a6b83e47a8
Thullyoo <thullyofilho@gmail.com>
2024-11-30 17:01:34 -0300

    feat: add home

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  showInput: boolean = false; 
+
+  toggleInput(): void {
+    this.showInput = !this.showInput; 
+  }
+}
