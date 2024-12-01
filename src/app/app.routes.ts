800e867df46f2d745366c4a90cc3f2c4a040ecc5
Thullyoo <thullyofilho@gmail.com>
2024-12-01 14:37:35 -0300

    feat: add path for login and add verification on home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthServiceService } from './services/auth-service.service';
 
 export const routes: Routes = [
+  { 
+    path: 'home',
+     component: HomeComponent, 
+     canActivate: [AuthServiceService] 
+  }, 
   {
-    path: "home",
-    component: HomeComponent
+    path: "login",
+    component: LoginComponent
   }
 ];
