265e91c3b1ee422d3a50f36f27503c78e7025eb0
Thullyoo <thullyofilho@gmail.com>
2024-12-02 14:54:24 -0300

    feat: add cards in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, type OnInit } from '@angular/core';
+import { ProductServiceService } from '../../services/product-service.service';
+import type Product from '../../interfaces/ProductInterface';
+import type { Observable } from 'rxjs';
+import { CardHomeComponent } from "../../components/card-home/card-home.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  private productService = inject(ProductServiceService);
+
+  public products: Product[] = [];
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+      {
+        next: response => {
+          this.products = response;
+          console.log(this.products);
+        },
+        error: err => {
+          console.error(err);
+        }
+      }
+    );
+  }
   
+
+
+
+
 }
