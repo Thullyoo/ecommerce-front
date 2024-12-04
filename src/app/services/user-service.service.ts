f418abdc104811aa6e6824ed9decac8cff0919af
Thullyoo <thullyofilho@gmail.com>
2024-12-04 12:43:08 -0300

    feat: add user service and method get user by id

---
+++ src/app/services/user-service.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { AuthServiceService } from './auth-service.service';
+import type UserGetById from '../interfaces/UserGetByIdInterface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  private url = "http://localhost:8080/user";
+
+  private httpClient = inject(HttpClient);
+
+  private authService = inject(AuthServiceService);
+
+  getUserById(){
+    
+    let token = this.authService.getToken();
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+    });
+
+    return this.httpClient.get<UserGetById>(this.url + "/infoId", {headers});
+  }
+}
