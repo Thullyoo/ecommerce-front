ebc7332b85b44b035fccf5e89050821037a69ce4
Thullyoo <thullyofilho@gmail.com>
2024-12-05 13:14:29 -0300

    feat: add verification on path /login and create pathj to register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,47 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthServiceService } from './services/auth-service.service';
 import { SellComponent } from './pages/sell/sell.component';
 import { UserPageComponent } from './pages/user-page/user-page.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent,
+    canActivate: [
+      () => {
+        const router = new Router();
+        const authService = new AuthServiceService();
+        if (authService.isTokenExpired()) {
+          return true;  
+        } else {
+          return router.navigateByUrl('/home');
+        }
+      }
+    ]
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [
+      () => {
+        const router = new Router();
+        const authService = new AuthServiceService();
+        if (authService.isTokenExpired()) {
+          return true;  
+        } else {
+          return router.navigateByUrl('/home');
+        }
+      }
+    ]
+  },
   { 
     path: 'home',
      component: HomeComponent, 
      canActivate: [AuthServiceService] 
   }, 
-  {
-    path: "login",
-    component: LoginComponent
-  },
   {
     path: "sell",
     component: SellComponent,
